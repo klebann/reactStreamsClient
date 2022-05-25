a8071d360517618365e18169ee549885c51f02e2
Jakub Kleban <jakub.kleban@enovation.ie>
2022-05-25 17:57:27 +0200

    Navigation with React Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-
-const PageOne = () => {
-  return <div>PageOne</div>;
-}
-
-const PageTwo = () => {
-  return (
-    <div>
-      PageTwo
-      <button>Click Me!</button>
-    </div>
-  );
-}
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import Header from './Header';
 
 const App = () =>  {
   return(
-    <div>
+    <div className="ui component">
       <BrowserRouter>
         <div>
-          <Route path="/" exact component={PageOne}/>
-          <Route path="/pagetwo" component={PageTwo}/>
+          <Header />
+          <Route path="/" exact component={StreamList}/>
+          <Route path="/streams/new" component={StreamCreate}/>
+          <Route path="/streams/edit" component={StreamEdit}/>
+          <Route path="/streams/delete" component={StreamDelete}/>
+          <Route path="/streams/show" component={StreamShow}/>
         </div>
       </BrowserRouter>
     </div>
