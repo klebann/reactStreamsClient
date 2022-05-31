c76c7a9794fbf1ef3c88866312ef4614d380f0fa
Jakub Kleban <jakub.kleban@enovation.ie>
2022-05-31 17:36:31 +0200

    Render authentication status

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,19 +1,33 @@
 import React, {Component} from 'react';
 
 class GoogleAuth extends Component {
+  state = { isSignedIn: null };
+
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
       window.gapi.client.init({
         clientId: '813325091323-5uk7rmai44cafl9pmb0s016487u5n9up.apps.googleusercontent.com',
         scope: 'email',
         plugin_name: "Tvitch"
+      }).then(() => {
+        this.auth = window.gapi.auth2.getAuthInstance();
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() });
       });
-
     });
   }
 
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>I dont know if we are signed in</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>I am signed in!</div>;
+    } else {
+      return <div>I am not signed in</div>;
+    }
+  }
+
   render() {
-    return <div>Google Auth</div>;
+    return <div>{this.renderAuthButton()}</div>;
   }
 }
 
