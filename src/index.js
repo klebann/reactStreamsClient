9a4af2f8ee272b03ef2253e3f81234cb9a2630dd
Jakub Kleban <jakub.kleban@enovation.ie>
2022-06-01 14:45:09 +0200

    Add redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import App from './components/App';
+import reducers from './reducers';
+
+const store = createStore(reducers);
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
-  <App tab="home" />
+  <Provider store={store}>
+    <App tab="home" />
+  </Provider>
 );
